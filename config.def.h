b1f90381d47ec118ff91ef8c62d74b80c3333d21
Justin Bogner <mail@justinbogner.com>
2013-06-06 19:11:40 -0600

    config: Don't resize the font with shift

--- config.def.h
+++ config.def.h
@@ -97,8 +97,8 @@ static Mousekey mshortcuts[] = {
 
 static Shortcut shortcuts[] = {
 	/* modifier		key		function	argument */
-	{ MODKEY|ShiftMask,	XK_Prior,	xzoom,		{.i = +1} },
-	{ MODKEY|ShiftMask,	XK_Next,	xzoom,		{.i = -1} },
+	{ MODKEY,		XK_Prior,	xzoom,		{.i = +1} },
+	{ MODKEY,		XK_Next,	xzoom,		{.i = -1} },
 	{ ShiftMask,		XK_Insert,	selpaste,	{.i =  0} },
 	{ MODKEY|ShiftMask,	XK_Insert,	clippaste,	{.i =  0} },
 	{ MODKEY,		XK_Num_Lock,	numlock,	{.i =  0} },
